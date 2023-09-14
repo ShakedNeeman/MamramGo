e40da594774a85b228ca973b2660a8bcd34c505e
GuyWeitzman <guyweitzman100@gmail.com>
2023-09-14 16:58:06 +0300

    Display the srevices and the drivers on safe modes

---
+++ Services&Drivers_on_Safemodes/Services&Drivers_on_Safemodes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Management;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Displaying Running Services...");
+        Console.WriteLine("===============================================");
+        ServiceDisplay serviceDisplay = new ServiceDisplay();
+        int serviceCount = serviceDisplay.Display();
+
+        Console.WriteLine("\n\nDisplaying Running System Drivers...");
+        Console.WriteLine("===============================================");
+        SystemDriverDisplay driverDisplay = new SystemDriverDisplay();
+        int driverCount = driverDisplay.Display();
+
+        Console.WriteLine($"\nTotal Running Services: {serviceCount}");
+        Console.WriteLine($"Total Running System Drivers: {driverCount}");
+    }
+}
---
+++ Services&Drivers_on_Safemodes/Services&Drivers_on_Safemodes/SystemDriverDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Management;
+using System;
+using System.IO;
+
+public class SystemDriverDisplay
+{
+    public int Display()
+    {
+        int count = 0;
+
+        // create management scope object
+        ManagementScope scope = new ManagementScope("\\\\.\\ROOT\\cimv2");
+
+        // create object query
+        ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_SystemDriver WHERE State = 'Running'");
+
+        // create object searcher
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+        // get a collection of WMI objects
+        ManagementObjectCollection queryCollection = searcher.Get();
+
+        // enumerate the collection.
+        foreach (ManagementObject m in queryCollection)
+        {
+            // access properties of the WMI object
+            string fullPath = m["PathName"]?.ToString();
+            string directory;
+            string fileName;
+
+            try
+            {
+                directory = fullPath != null ? Path.GetDirectoryName(fullPath) : "N/A";
+                fileName = fullPath != null ? Path.GetFileName(fullPath) : "N/A";
+            }
+            catch (ArgumentException)
+            {
+                directory = "Error: Invalid path format";
+                fileName = "Error: Invalid path format";
+            }
+
+            count++;
+            Console.WriteLine($"Driver Name   : {m["Name"]}");
+            Console.WriteLine($"Path          : {directory}");
+            Console.WriteLine($"File Name     : {fileName}");
+            Console.WriteLine("---------------------------------------");
+        }
+        return count;
+    }
+}
